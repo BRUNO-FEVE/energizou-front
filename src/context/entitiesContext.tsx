9f14abd29dab53c2a60dbd95ed567adc88886f7e
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-11-05 04:58:31 -0300

    feat: add `entities context`
    add entities context to hold user and companies global data

---
+++ src/context/entitiesContext.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { PropsWithChildren, createContext, useState } from "react";
+
+export interface userData {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+export interface companyData {
+  cnpj: string;
+  name: string;
+  cep: number;
+  address: string;
+  address_number: number;
+  phone: string;
+  user?: userData;
+}
+
+interface DefaultEntitiesProps {
+  user: userData | null;
+  company: companyData | null;
+  setUser: (user: userData) => void;
+  setCompany: (company: companyData) => void;
+}
+
+const DefaultEntities: DefaultEntitiesProps = {
+  user: null,
+  company: null,
+  setUser: (_user: userData) => {
+    return;
+  },
+  setCompany: (_company: companyData) => {
+    return;
+  },
+};
+
+export const EntitiesContext = createContext(DefaultEntities);
+
+export function EntitiesProvider({ children }: PropsWithChildren) {
+  const [user, setUser] = useState<userData | null>(null);
+  const [company, setCompany] = useState<companyData | null>(null);
+
+  return (
+    <EntitiesContext.Provider value={{ user, company, setUser, setCompany }}>
+      {children}
+    </EntitiesContext.Provider>
+  );
+}
