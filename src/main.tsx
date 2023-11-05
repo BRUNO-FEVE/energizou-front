b31bb27bc69aff6ad5ff86b1250af6817a10df26
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-11-05 04:51:50 -0300

    feat: add `navigation` between pages
    react router dom config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.less";
-import LoginPage from "./pages/LoginPage/LoginPage";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./router";
+import { EntitiesProvider } from "./context/entitiesContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <LoginPage />
+    <EntitiesProvider>
+      <Router>
+        <AppRouter />
+      </Router>
+    </EntitiesProvider>
   </React.StrictMode>
 );
---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ResgisterPage from "./pages/RegisterPage/ResgisterPage";
+import HomePage from "./pages/HomePage/HomePage";
+import CompanyPage from "./pages/CompanyPage/CompanyPage";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<ResgisterPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/company" element={<CompanyPage variant="update" />} />
+    </Routes>
+  );
+}
